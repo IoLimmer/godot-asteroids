f7831089594ff48b7aa9315883e5afacec81b2e1
IoLimmer <jh20006@bristol.ac.uk>
2023-11-18 20:16:40 +0000

    Implemented lerp to angular velocity

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,13 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const ROTATION_SPEED = 1.5
-const LERP = .05
 
-var rotation_direction = 0
+const SPEED_LERP = .05
+const ROTATION_SPEED_LERP = .2
+
+var rotation_direction = 0.0
+
 
 func get_input():
-	rotation_direction = Input.get_axis("ui_left", "ui_right")
-	self.velocity = lerp(self.velocity, transform.x * Input.get_axis("ui_down", "ui_up") * SPEED, LERP)
+	rotation_direction = lerp(rotation_direction, Input.get_axis("ui_left", "ui_right"), ROTATION_SPEED_LERP)
+	self.velocity = lerp(self.velocity, transform.x * Input.get_axis("ui_down", "ui_up") * SPEED, SPEED_LERP)
 
 func _physics_process(delta):
 	get_input()
