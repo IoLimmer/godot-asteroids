3f503678903f7ffc142b7677f1e8b5fa893cce67
IoLimmer <jh20006@bristol.ac.uk>
2023-11-18 17:51:47 +0000

    Added asteroids-style movement

---
+++ scripts/Player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const ROTATION_SPEED = 1.5
+
+var rotation_direction = 0
+
+func get_input():
+	rotation_direction = Input.get_axis("ui_left", "ui_right")
+	self.velocity = transform.x * Input.get_axis("ui_down", "ui_up") * SPEED
+
+func _physics_process(delta):
+	get_input()
+	self.rotation += rotation_direction * ROTATION_SPEED * delta
+	move_and_slide()
