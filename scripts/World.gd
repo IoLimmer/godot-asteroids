26749524d0fd9eb0209de1c6aa44807b5e6397c8
IoLimmer <jh20006@bristol.ac.uk>
2023-11-18 19:42:51 +0000

    wraparound accounts for size of object's sprite

--- scripts/World.gd
+++ scripts/World.gd
@@ -6,18 +6,17 @@ var SCREEN_HEIGHT = ProjectSettings.get_setting("display/window/size/viewport_he
 
 
 func check_child_position(child):
-	if child.name == "Player":
-		print(child.position)
+	var sprite_width = round(child.get_child(0).texture.get_width() * child.scale.x)
 		
-	if child.position.x > SCREEN_WIDTH:
-		child.position.x -= SCREEN_WIDTH
-	elif child.position.x < 0:
-		child.position.x += SCREEN_WIDTH
+	if child.position.x > SCREEN_WIDTH + sprite_width:
+		child.position.x -= SCREEN_WIDTH + sprite_width
+	elif child.position.x < 0 - sprite_width:
+		child.position.x += SCREEN_WIDTH + sprite_width
 		
-	if child.position.y > SCREEN_HEIGHT:
-		child.position.y -= SCREEN_HEIGHT
-	elif child.position.y < 0:
-		child.position.y += SCREEN_HEIGHT
+	if child.position.y > SCREEN_HEIGHT + sprite_width:
+		child.position.y -= SCREEN_HEIGHT + sprite_width
+	elif child.position.y < 0 - sprite_width:
+		child.position.y += SCREEN_HEIGHT + sprite_width
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
