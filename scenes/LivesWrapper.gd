be90be02c2d0f6ca6c676737a9c307f1c34d76da
IoLimmer <jh20006@bristol.ac.uk>
2023-12-20 23:16:56 +0000

    Added sprites to HUD that show number of remaining lives

---
+++ scenes/LivesWrapper.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+@onready var sprites = self.get_children()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	print(sprites);
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	for i in range(Utils.lives-1):
+		sprites[i].set_visible(true)
+		
+	if Utils.lives < 3:
+		for i in range(2, Utils.lives-1, -1):
+			sprites[i].set_visible(false)
+
