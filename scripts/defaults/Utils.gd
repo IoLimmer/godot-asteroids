776a519cfda47bd6b2b73d402e182fdec98359a5
Io Limmer <iolimmer@outlook.com>
2024-01-05 14:03:24 +0000

    Update Utils.gd
    Utils.gd has default score, lives and running now

--- scripts/defaults/Utils.gd
+++ scripts/defaults/Utils.gd
@@ -1,19 +1,23 @@
 extends Node
 
 
-var score = 0
-var lives = 3
+const default_score = 0
+const default_lives = 3
+const default_running = true
 
-var running = true
+var score
+var lives
+var running
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	reset()
+#	pass # Replace with function body.
 	
 func reset():
-	score = 0
-	lives = 3
-	running = true
+	score = default_score
+	lives = default_lives
+	running = default_running
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
